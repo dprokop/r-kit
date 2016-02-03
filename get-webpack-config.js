e5f35fd05c8ef84810bb6cf898b6d59cca3adf6d
Dominik Prokop <prokopd@gmail.com>
2016-02-03 22:42:22 +0100

    build: Webpack config generator

--- get-webpack-config.js
+++ get-webpack-config.js
@@ -5,19 +5,55 @@ var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var stylesETP = new ExtractTextPlugin(1, 'main.css', {allChunks: true});
+var env = {
+    DEV: 'development',
+    PROD: 'production'
+}
 
-var plugins = [stylesETP];
-
-var autoprefixerBrowsers = ['last 2 versions']
+var defaultOptions = {
+    env: process.env.NODE_ENV,
+    autoprefixerBrowsers: ['last 2 versions']
+}
 
 module.exports = function (options) {
 
-    if(options.uglify) {
+    var settings = Object.assign({}, defaultOptions, options);
+    var stylesETP = new ExtractTextPlugin('main.css', {allChunks: true});
+    var vendorStylesETP = new ExtractTextPlugin('vendors.css', {allChunks: true});
+
+
+    var jsLoaders = [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules)/,
+            loader: settings.reactHot ? 'react-hot-loader!babel-loader?presets[]=react,presets[]=es2015!eslint-loader?fix=true' : 'babel-loader?presets[]=react,presets[]=es2015!eslint-loader'
+        },
+        {
+            test: require.resolve('react'), loader: 'expose?React'
+        },
+        {
+            test: require.resolve('react-dom'), loader: 'expose?ReactDOM'
+        }
+    ]
+
+    var styleLoaders = [
+        {
+            test: /main.scss$/,
+            loader: settings.env === env.DEV ? 'style-loader!css-loader?sourceMap&importLoaders=1!postcss-loader!sass-loader?sourceMap' : stylesETP.extract('style-loader', 'css-loader?minimize&importLoaders=1!postcss-loader!sass-loader'),
+        },
+        {
+            test: /(node_modules)*\.(css)$/,
+            loader: settings.env === env.DEV ? 'style-loader!css-loader' : vendorStylesETP.extract('style-loader', 'css-loader?minimize'),
+        },
+    ]
+
+    var plugins = (settings.env === env.DEV ? [] : [stylesETP, vendorStylesETP])
+
+    if(settings.uglify) {
         plugins.push(new webpack.optimize.UglifyJsPlugin({
             sourceMap: true,
             mangle: {
-                except: [] // add names that you don't want to be mangled
+                except: settings.uglifyMangleExcept ? settings.uglifyMangleExcept : []
             }
         }));
     }
@@ -38,22 +74,10 @@ module.exports = function (options) {
             publicPath: '/dist/'
         },
 
-        devtool: 'source-map',
+        devtool: settings.devtool,
 
         module: {
-            loaders: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /(node_modules)/,
-                    loader: 'react-hot-loader!babel-loader?presets[]=react,presets[]=es2015!eslint-loader?fix=true',
-                },
-                {
-                    test: /main.scss$/,
-                    loader: stylesETP.extract('style-loader', 'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]&sourceMap&importLoaders=1!postcss-loader!sass-loader?sourceMap'),
-                },
-                { test: require.resolve('react'), loader: 'expose?React' },
-                { test: require.resolve('react-dom'), loader: 'expose?ReactDOM' }
-            ],
+            loaders: Array.prototype.concat(jsLoaders, styleLoaders),
         },
 
         resolve: {
@@ -61,11 +85,8 @@ module.exports = function (options) {
             root: [path.resolve(__dirname, 'src')]
         },
 
-        postcss: [autoprefixer({browsers: autoprefixerBrowsers})],
+        postcss: [autoprefixer({browsers: settings.autoprefixerBrowsers})],
 
         plugins: plugins,
     };
 };
-
-
-
