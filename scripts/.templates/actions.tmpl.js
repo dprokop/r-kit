0adf8b82a131fe49c88fc2233b0259dcf879aa7a
Dominik Prokop <prokopd@gmail.com>
2016-02-15 01:44:29 +0100

    tools: Areas factory added
    - scripts/createArea.js allows quick creation of new area
    - templates for factory located in scripts/.templates

---
+++ scripts/.templates/actions.tmpl.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/*=======================================
+=              Action types             =
+=======================================*/
+/**
+ * [Action name] action type
+ * @todo Define AREA NAME action types
+ * @constant
+ * @type {[type]}
+ */
+export const ACTION_NAME = 'ACTION_NAME'
+
+/*=====  End of Action types  ======*/
+
+
+/*=======================================
+=            Action creators            =
+=======================================*/
+
+/** @module <%= areaName %>/actions */
+
+/**
+ * [Action name] action creator
+ * @todo Implement action creator
+ * @param {[type]} payload [description]
+ * @return {@link _ACTION_NAME} action object
+ */
+export function actionName (payload) {
+    return {
+        type: ACTION_NAME,
+        payload
+    }
+}
+
+/*=====  End of Action creators  ======*/
+
