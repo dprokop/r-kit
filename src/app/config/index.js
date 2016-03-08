5ebb71ab76708c553d40c1694de0dfaefc691d7f
Dominik Prokop <prokopd@gmail.com>
2016-03-08 09:53:54 +0100

    feat: App config file
    Purpose of this file is to store shared config for the app. E.g. integration points API keys

---
+++ src/app/config/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+export default {
+    services: {
+        OpenWeather: {
+            appId: '6058764b2c589f639754a906a4dfb561'
+        },
+        Sentry: {
+            clientUrl: 'https://33b34862e1cc45f8b863662d9d9b7415@app.getsentry.com/69634'
+        }
+    }
+}
