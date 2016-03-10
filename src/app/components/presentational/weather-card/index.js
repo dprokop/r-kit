afe2e54e6f7b7f7892c6a86243b3f9b3bc32ad61
Dominik Prokop <prokopd@gmail.com>
2016-03-10 16:58:14 +0100

    feat: Updated rendering of weather card and weather cards list

--- src/app/components/presentational/weather-card/index.js
+++ src/app/components/presentational/weather-card/index.js
@@ -3,34 +3,52 @@
 import { PropTypes } from 'react'
 
 
-var WeatherCard = ({title, summary, onRefresh}) => {
-    return (
-        <div className="demo-card-wide mdl-card mdl-shadow--2dp">
-            <div className="mdl-card__title">
-                <h2 className="mdl-card__title-text">{title}</h2>
+var WeatherCard = ({isLoading, title, summary, onRefresh}) => {
+    var card
+    if(isLoading){
+        card = (
+            <div className="demo-card-wide mdl-card mdl-shadow--2dp">
+                <div className="mdl-card__title">
+                    <h2 className="mdl-card__title-text">Loading</h2>
+                </div>
             </div>
-            <div className="mdl-card__supporting-text">
-                {summary}
+        )
+    } else {
+        card = (
+            <div className="demo-card-wide mdl-card mdl-shadow--2dp">
+                <div className="mdl-card__title">
+                    <h2 className="mdl-card__title-text">{title}</h2>
+                </div>
+                <div className="mdl-card__supporting-text">
+                    {summary}
+                </div>
+                <div className="mdl-card__actions mdl-card--border">
+                    <a onClick={ () => onRefresh() } className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                      Refresh
+                    </a>
+                </div>
+                <div className="mdl-card__menu">
+                    <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+                        <i className="material-icons"></i>
+                    </button>
+                </div>
             </div>
-            <div className="mdl-card__actions mdl-card--border">
-                <a onClick={ () => onRefresh() } className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                  Refresh
-                </a>
-            </div>
-            <div className="mdl-card__menu">
-                <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-                    <i className="material-icons"></i>
-                </button>
-            </div>
-        </div>
-    )
+        )
+    }
+
+    return card
 }
 
 WeatherCard.propTypes = {
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
+    id: PropTypes.number,
+    title: PropTypes.string,
+    summary: PropTypes.string,
     onRefresh: PropTypes.func
+}
+
+WeatherCard.defaultProps = {
+    title: 'Card title',
+    summary: 'Card summary'
 }
 
 export default WeatherCard
--- src/app/components/presentational/weather-cards-list/index.js
+++ src/app/components/presentational/weather-cards-list/index.js
@@ -2,20 +2,22 @@
 
 import WeatherProvider from 'components/containers/weather-provider'
 import WeatherCard from 'components/presentational/weather-card'
-import _ from 'underscore'
 import { PropTypes } from 'react'
+import _ from 'underscore'
 
-var WeatherCardsList = ({ channels, onChannelRefresh, onRefresh }) => {
-    var cards = _.map(channels, function (channel, i) {
-        return (
-            <WeatherCard
-                id={channel.id}
-                title = {channel.title}
-                summary = {channel.summary}
-                onRefresh = { () => onChannelRefresh(channel.id) }
-                key={channel.id}
-            />
-        )
+import Services from 'common/services'
+
+var WeatherCardsList = ({ channelsData, onChannelRefresh, onRefresh }) => {
+    var cards = _.map(_.keys(channelsData), key => {
+        var card, data
+        if(channelsData[key].isLoading){
+            card = <WeatherCard isLoading key={key} />
+        }else {
+            data = channelsData[key].data
+            card = <WeatherCard id={data.id} title={data.name} key={data.id} />
+        }
+
+        return card
     })
 
     return (
@@ -29,7 +31,7 @@ var WeatherCardsList = ({ channels, onChannelRefresh, onRefresh }) => {
 }
 
 WeatherCardsList.propTyp = {
-    channels: PropTypes.array,
+    channelsData: PropTypes.object,
     onRefresh: PropTypes.func,
     onChannelRefresh: PropTypes.func
 }
