dc7bd2fdf6875ce6eba4c488291fb617d0e6d42a
Dominik Prokop <prokopd@gmail.com>
2016-03-18 16:35:30 +0100

    example: Updated weather reducer reference

--- src/app/config/reducers.js
+++ src/app/config/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import { weather as weatherReducer } from 'areas/weather/reducer'
+import weatherReducer from 'areas/weather/reducer'
 
 export default combineReducers({
   weather: weatherReducer
